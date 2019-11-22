c7162301541e3516b3049aed53533f03ecb11edd
Steve VandenBrink <steve@vandenbrinksoftware.com>
2019-11-22 17:28:00 -0500

    Added more user controller tests

--- PokerPlanner.API.Tests/Controllers/UsersControllerTests.cs
+++ PokerPlanner.API.Tests/Controllers/UsersControllerTests.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoWrapper.Wrappers;
 using MongoDB.Bson;
 using Moq;
 using PokerPlanner.API.Controllers;
 using PokerPlanner.Entities.Domain.Mongo;
+using PokerPlanner.Entities.DTO;
 using PokerPlanner.Repositories.Interfaces.Domain.Mongo;
 using Xunit;
 
@@ -12,12 +17,14 @@ namespace PokerPlanner.API.Tests.Controllers
     public class UsersControllerTests
     {
         private readonly Mock<IUserRepository> _mockUserRepo;
+        private readonly Mock<IMapper> _mockMapper;
         private readonly UsersController _controller;
 
         public UsersControllerTests()
         {
             _mockUserRepo = new Mock<IUserRepository>();
-            _controller = new UsersController(null, null, _mockUserRepo.Object);
+            _mockMapper = new Mock<IMapper>();
+            _controller = new UsersController(_mockMapper.Object, null, _mockUserRepo.Object);
         }
         [Fact]
         public async Task GetUserByIdTest()
@@ -32,6 +39,51 @@ namespace PokerPlanner.API.Tests.Controllers
 
             Assert.IsType<User>(result);
             Assert.Equal(objectId, result.Id);
+        }
+
+        [Fact]
+        public async Task GetAllUsersTest()
+        {
+            _mockUserRepo.Setup(repo => repo.GetUsers()).ReturnsAsync(new List<User> { new User(), new User(), new User() });
+
+            var result = await _controller.GetAllUsers();
+
+            Assert.IsType<List<User>>(result);
+            Assert.Equal(3, result.Count());
+        }
+
+        [Fact]
+        public async Task NewUserInvalidModelTest()
+        {
+            var newUserDto = new UserDTO();
+
+            _controller.ModelState.AddModelError("error", "error");
+
+            try
+            {
+                var result = await _controller.NewUser(newUserDto);
+            }
+            catch (Exception e)
+            {
+                Assert.IsType<ApiException>(e);
+            }
+        }
+
+        [Fact]
+        public async Task NewUserSuccessfullyCreatedTest()
+        {
+            var newUserDto = new UserDTO();
+            var mappedUser = new User();
+
+            _mockMapper.Setup(map => map.Map<User>(newUserDto)).Returns(mappedUser);
+            _mockUserRepo.Setup(repo => repo.AddNewUser(mappedUser)).ReturnsAsync(mappedUser);
+
+            var result = await _controller.NewUser(newUserDto);
+
+            Assert.IsType<ApiResponse>(result);
+            Assert.Equal(201, result.StatusCode);
+            Assert.IsType<User>(result.Result);
+            Assert.Equal(mappedUser, result.Result);
         }
     }
 }
