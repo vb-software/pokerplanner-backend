2b6fb5595129101b6ad300b9ffb098fa06401ad9
Steve VandenBrink <steve@vandenbrinksoftware.com>
2019-11-22 15:52:05 -0500

    Added one test to see if coverage works

--- PokerPlanner.API.Tests/Controllers/UsersControllerTests.cs
+++ PokerPlanner.API.Tests/Controllers/UsersControllerTests.cs
@@ -1,14 +1,37 @@
 using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using Moq;
+using PokerPlanner.API.Controllers;
+using PokerPlanner.Entities.Domain.Mongo;
+using PokerPlanner.Repositories.Interfaces.Domain.Mongo;
 using Xunit;
 
 namespace PokerPlanner.API.Tests.Controllers
 {
-    public class UnitTest1
+    public class UsersControllerTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly Mock<IUserRepository> _mockUserRepo;
+        private readonly UsersController _controller;
 
+        public UsersControllerTests()
+        {
+            _mockUserRepo = new Mock<IUserRepository>();
+            _controller = new UsersController(null, null, _mockUserRepo.Object);
+        }
+        [Fact]
+        public async Task GetUserByIdTest()
+        {
+            User mockUser = new User();
+            var objectId = ObjectId.GenerateNewId().ToString();
+            _mockUserRepo.Setup(repo => repo.GetUserById(ObjectId.Parse(objectId))).ReturnsAsync(mockUser);
+
+            mockUser.Id = objectId;
+
+            var result = await _controller.GetUserById(objectId);
+
+            Assert.IsType<User>(result);
+            Assert.Equal(objectId, result.Id);
         }
     }
 }
